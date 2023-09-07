40ccb8486014be86d740dc356f8475c9bcc13fc0
samade747 <samad.e747@gmail.com>
2023-09-07 23:43:56 +0500

    final update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // array push() --> Insert an element at the end // push end add
 function pushEndAdd() {
     const array = [1, 2, 3];
-    array.push(4);
+    array.push(parseInt(prompt("Enter a number:")));
     //result 1, 2, 3, 4,
     document.getElementById("pushResult").innerHTML = JSON.stringify(array);
     }
@@ -11,10 +11,9 @@ function pushEndAdd() {
     //pop(): --> Remove an element from the end // pop remove end 
     function popRemoveEnd() {
     const array1 = [5, 6, 7, 8, 18];
-    array1.pop();
-    // result [5, 6, 7, 8}
-    array1.pop();
-    // result [5, 6, 7]
+    array1.pop(parseInt(prompt("Enter a number:")));
+   
+    
     document.getElementById('popResult').innerHTML = JSON.stringify(array1);
     }
     
@@ -24,7 +23,7 @@ function pushEndAdd() {
     //unshift() inserts an element in beginning //  unshift start add
     function unshiftInsertBeginning(){
     const array2 = [11, 12];
-    array2.unshift(8, 9, 10);
+    array2.unshift(parseInt(prompt("Enter a number:")));
     // result [8, 9, 10, 11, 12]
     document.getElementById('unshiftResult').innerHTML = JSON.stringify(array2);
     }
@@ -34,8 +33,8 @@ function pushEndAdd() {
     //shift() remove first element //  shift remove start
     function shiftRemoveStart(){
     const array3 = [12, 12, 12, 13, 14];
-    array3.shift();
-    array3.shift();
+    array3.shift(parseInt(prompt("Enter a number:")));
+    
     // result is  [12, 13, 14]
     document.getElementById('shiftremovestartresult').innerHTML = JSON.stringify(array3);
     }
